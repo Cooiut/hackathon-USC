37aebd5f3f50f4cab66652d2cf8523c81a4e79ee
Haphaistos2333 <Haphaistos2333@hotmail.com>
2020-09-05 02:35:01 -0700

    re-design activity

--- Server/activity.py
+++ Server/activity.py
@@ -1,9 +1,18 @@
 import datetime
+import time
 import random
+import string
 
 import flask_login
 import json
 import os.path
+
+
+def rand_id():
+    while res := ''.join(random.sample(string.ascii_lowercase + string.digits, 6)):
+        if not os.path.exists(f'data/activities/{res}.json'):
+            break
+    return res
 
 
 class Activity:
@@ -11,8 +20,8 @@ class Activity:
         self.act_id = act_id
         self.name = name
         self.info = info
-        self.curr_users = curr_users
-        self.auth_users = auth_users
+        self.curr_users = curr_users  # {username: last checkin, # of checkin}
+        self.left_users = left_users  # {username: leave time}
         self.duration = duration
         self.frequency = frequency  # per day or week
 
@@ -24,40 +33,22 @@ class Activity:
 
     @classmethod
     def new_activity(cls, name, info, durations, frequency):
+        return cls(rand_id(), name, info, durations, {}, {}, frequency).__save()
 
-        return cls(random.randint(0, 1000), name, info, durations, {}, frequency).__save()
-
-    def add_user(self, username):
-        if username in self.users.keys():
-            print("You have already enrolled in this activity!")
-        else:
-            self.users[username] = [datetime.datetime.today() + datetime.timedelta(days=self.frequency), self.duration]
+    def user_join(self, user):
+        self.curr_users[user] = [time.time(), 0]
         return self.__save()
 
-    # TODO
-
-    def remove_user(self, username):
-        self.users.pop(username)
+    def user_leave(self, user):
+        self.curr_users.pop(user)
+        self.left_users[user] = time.time()
         return self.__save()
 
-    def clock_in(self, username):
-        self.users[username][0] = datetime.datetime.today() + datetime.timedelta(days=self.frequency)
-        if self.users[username][1] - 1 == 0:
-            print("完成所有打卡！")
-        else:
-            self.users[username][1] -= 1
-            print("完成打卡！")
-        return self.__save()
+    def user_checkin(self):
+        pass
+        # TODO
 
-    # TODO
-
-    def check(self):
-        for x in self.users:
-            if datetime.datetime.today() > self.users[x][0]:
-                print("错过打卡！")
-                self.remove_user(x)
-        return self.__save()
-    # TODO
+    # TODO: check expiration
 
 
 if __name__ == '__main__':
